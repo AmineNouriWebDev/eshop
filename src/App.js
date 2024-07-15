5f001d37e786cf53c1362a03122772fc748d8246
AmineNouriWebDev <macbookair@MacBook-Air-de-macbookair.local>
2024-07-15 22:28:55 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import './App.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './header/Header';
-import Navbar from './header/Navbar';
-
-
-
+import "./App.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./header/Header";
+import Navbar from "./header/Navbar";
+import Carousel from "./header/Carousel";
 
 function App() {
   return (
     <div className="App">
-    <header> 
-    <Header />
-    </header>
-    <nav>
-    <Navbar />
-    </nav>
-    <section>
+      <header>
+        <Header />
+      </header>
       <div>
-
+        <Navbar />
       </div>
-    </section>
-    <footer></footer>
+      <section>
+        <div>
+          <Carousel />
+        </div>
+      </section>
+      <footer></footer>
     </div>
   );
 }
--- src/header/Navbar.jsx
+++ src/header/Navbar.jsx
@@ -1,44 +1,81 @@
-import React from 'react';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import React from "react";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 
 class Example extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-        dropdownOpen: false
-        };
-    }
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false,
+    };
+  }
 
-    toggle() {
-        this.setState(prevState => ({
-        dropdownOpen: !prevState.dropdownOpen
-        }));
-    }
+  toggle() {
+    this.setState((prevState) => ({
+      dropdownOpen: !prevState.dropdownOpen,
+    }));
+  }
 
-    render() {
-        return (
-        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+  render() {
+    return (
+      <div class="btn-category-container">
+        <div className="btn-category">
+          <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
             <DropdownToggle caret>
-            <i class="bi bi-list"></i>
-            Catégories
+              <i class="bi bi-list"></i>
+              Catégories
             </DropdownToggle>
             <DropdownMenu>
-            <DropdownItem header>Appareils électroniques</DropdownItem>
-            <DropdownItem>Ordinateurs</DropdownItem>
-            <DropdownItem>Smartphones</DropdownItem>
-            <DropdownItem>Tablettes</DropdownItem>
-            <DropdownItem header>Electroménager</DropdownItem>
-            <DropdownItem>Téléviseurs</DropdownItem>
-            <DropdownItem>Pour la cuisine</DropdownItem>
-            <DropdownItem>AUtres</DropdownItem>
+              <DropdownItem header>Appareils électroniques</DropdownItem>
+              <DropdownItem>Ordinateurs</DropdownItem>
+              <DropdownItem>Smartphones</DropdownItem>
+              <DropdownItem>Tablettes</DropdownItem>
+              <DropdownItem header>Electroménager</DropdownItem>
+              <DropdownItem>Téléviseurs</DropdownItem>
+              <DropdownItem>Pour la cuisine</DropdownItem>
+              <DropdownItem>AUtres</DropdownItem>
             </DropdownMenu>
-
-
-        </Dropdown>
-        );
-    }
+          </Dropdown>
+        </div>
+        <div className="search-box">
+          <input
+            type="text"
+            placeholder={"Search..."}
+            className="header-search-input"
+          />
+          <button className="header-search-input-btn">
+            <i class="bi bi-search"></i>
+          </button>
+        </div>
+        <nav className="navbar">
+          <ul>
+            <li className="active">
+              <i class="bi bi-house"></i>
+              Home
+            </li>
+            <li>
+              <i class="bi bi-question-octagon"></i>
+              About
+            </li>
+            <li>
+              <i class="bi bi-chat-left"></i>
+              Contact
+            </li>
+            <li>
+              <i class="bi bi-door-open"></i>
+              Register
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default Example;
